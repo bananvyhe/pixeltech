0ddf79394c96cc5c91fd2f2b2d9acdaba1cdce9c
bananvyhe <loadonden@yahoo.com>
2018-07-23 01:41:06 +0500

    dummy

--- app/javascript/head.vue
+++ app/javascript/head.vue
@@ -1,6 +1,10 @@
 <template>
   <div id="head">
-    <p>{{ message }}</p>
+  	<div class="contain">
+    <p class="title">{{ message }}</p>
+     <p class="subj">{{subj}}</p> 
+     <p class="subj">{{cont}}</p> 
+    </div>
   </div>
 </template>
 
@@ -8,15 +12,37 @@
 export default {
   data: function () {
     return {
-      message: "Pixeltech"
+    	subj: 'Разработка веб-приложений, сайтов на Ruby on Rails + Vue.js',
+      message: "Pixeltech",
+      cont: "admin@pixeltech.ru"
     }
   }
 }
 </script>
-
 <style scoped>
-p {
+@import "_variables";
+#head {
+	display: flex;
+	height: 100vh;
+	align-items: center;
+	justify-content: center;
+}
+.contain {  
+	position: relative;
+	display: flex;
+	flex-direction: column;
+}
+.subj {display: flex;
+  color: $titleColor;
+	padding: 0em;
+	align-self: center;
+	font-size: 1em;
+}
+.title {
+	display: flex;
+	justify-content: center;
+	padding: 1em 1em 0.3em 1em;
+	color: $titleColor;
   font-size: 2em;
-  text-align: center;
 }
 </style>
--- app/javascript/packs/hello_vue.js
+++ app/javascript/packs/hello_vue.js
@@ -19,7 +19,6 @@ document.addEventListener('DOMContentLoaded', () => {
     el: '#head',
     render: h => h(Head)
   })
-  console.log(app)
 })
 
 
