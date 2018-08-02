31d037b07bee9b4f8e3e1944a148109679e7cdd9
bananvyhe <loadonden@yahoo.com>
2018-08-03 00:33:32 +0500

    1

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,9 +30,9 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
   config.action_mailer.default_url_options = { :host => 'pixeltech.ru' }
-  config.assets.precompile += Ckeditor.assets
-  config.assets.precompile += %w( ckeditor/* )
-  config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
+  # config.assets.precompile += Ckeditor.assets
+  # config.assets.precompile += %w( ckeditor/* )
+  # config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
   config.assets.js_compressor = :uglifier
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
