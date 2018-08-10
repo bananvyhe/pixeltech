aea773dffbeda8d12e086785baf20e2a68a2cc27
bananvyhe <loadonden@yahoo.com>
2018-08-10 19:17:10 +0500

    devise setuped

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,11 @@ module Pixeltech
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-
+		config.i18n.default_locale = :ru
+    config.i18n.locale = :ru
+    config.generators.system_tests = nil
+    config.generators.stylesheets = false
+    config.generators.javascripts = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
