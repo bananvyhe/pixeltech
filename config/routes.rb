4a4fac066f3347e9e915ab892dfb9434d20e095f
bananvyhe <loadonden@yahoo.com>
2018-08-19 19:01:25 +0500

    sorted list_teams setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  resources :cards
-  resources :lists
   devise_for :users, controllers: { registrations: 'registrations' }
   get 'landing_page/index'
   root 'landing_page#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :lists
+  resources :cards
 end
