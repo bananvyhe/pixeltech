920170f2ae27fc19bae8c7bff1589d1f42fe8a4e
bananvyhe <loadonden@yahoo.com>
2018-08-21 23:08:34 +0500

    draggable implementation

--- app/javascript/boar.vue
+++ app/javascript/boar.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <draggable>
     <div v-for="(list, index) in lists" class="cols">
       <h6>{{list.name}}</h6>
       <hr />
@@ -9,11 +9,13 @@
       <textarea v-model="messages[list.id]"></textarea>
       <button v-on:click="submitMessages(list.id)">Добавить</button>
     </div>
-  </div>
+  </draggable>
 </template>
 
 <script>
+  import draggable from 'vuedraggable'
   export default {
+    components: { draggable },
     props: ["original_lists"],
     data: function (){
       return {
