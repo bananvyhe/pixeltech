e17a4316837b04a32c6ca6638c999566697a3e38
bananvyhe <loadonden@yahoo.com>
2018-09-07 19:10:03 +0500

    regform sretup

--- app/javascript/components/registration.vue
+++ app/javascript/components/registration.vue
@@ -3,24 +3,33 @@
 		<div class="regplace">
 			<el-button size="mini" @click="dialogFormVisible = true">Регистрация</el-button>
 
-			<el-dialog title="Shipping address" :visible.sync="dialogFormVisible">
+			<el-dialog title="Создать аккаунт:" :visible.sync="dialogFormVisible">
 			  <el-form :model="form">
-			    <el-form-item label="Promotion name" :label-width="formLabelWidth">
-			      <el-input v-model="form.name" auto-complete="off"></el-input>
+			    <el-form-item size="mini" label="Емайл:" :label-width="formLabelWidth">
+			      <el-input v-model="form.mail" auto-complete="off"></el-input>
 			    </el-form-item>
-			    <el-form-item label="Zones" :label-width="formLabelWidth">
+			    <el-form-item size="mini" label="Имя пользователя:" :label-width="formLabelWidth">
+			      <el-input v-model="form.usernamne" auto-complete="off"></el-input>
+			    </el-form-item>
+			    <el-form-item size="mini" label="Пароль:" :label-width="formLabelWidth">
+			      <el-input v-model="form.password" auto-complete="off"></el-input>
+			    </el-form-item>
+			    <el-form-item size="mini" label="Пароль еще раз:" :label-width="formLabelWidth">
+			      <el-input v-model="form.password" auto-complete="off"></el-input>
+			    </el-form-item>
+			   <!--  <el-form-item label="Zones" :label-width="formLabelWidth">
 			      <el-select v-model="form.region" placeholder="Please select a zone">
 			        <el-option label="Zone No.1" value="shanghai"></el-option>
 			        <el-option label="Zone No.2" value="beijing"></el-option>
 			      </el-select>
-			    </el-form-item>
+			    </el-form-item> -->
 			  </el-form>
 			  <span slot="footer" class="dialog-footer">
-			    <el-button @click="dialogFormVisible = false">Cancel</el-button>
-			    <el-button type="primary" @click="dialogFormVisible = false">Confirm</el-button>
+			    <el-button size="mini" @click="dialogFormVisible = false">Отмена</el-button>
+			    <el-button size="mini" type="primary" @click="dialogFormVisible = false">Подтвердить</el-button>
 			  </span>
 			</el-dialog>
-			
+
 		</div>
 	</div>
 </template>
@@ -30,24 +39,8 @@
 export default {
 	data() {
       return {
-        gridData: [{
-          date: '2016-05-02',
-          name: 'John Smith',
-          address: 'No.1518,  Jinshajiang Road, Putuo District'
-        }, {
-          date: '2016-05-04',
-          name: 'John Smith',
-          address: 'No.1518,  Jinshajiang Road, Putuo District'
-        }, {
-          date: '2016-05-01',
-          name: 'John Smith',
-          address: 'No.1518,  Jinshajiang Road, Putuo District'
-        }, {
-          date: '2016-05-03',
-          name: 'John Smith',
-          address: 'No.1518,  Jinshajiang Road, Putuo District'
-        }],
-        dialogTableVisible: false,
+         
+        
         dialogFormVisible: false,
         form: {
           name: '',
@@ -59,7 +52,7 @@ export default {
           resource: '',
           desc: ''
         },
-        formLabelWidth: '120px'
+        formLabelWidth: '150px'
       };
     }
 }
