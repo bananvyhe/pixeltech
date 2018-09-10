f0c00268b3e1824e7e051b9211071aaea0450f1e
bananvyhe <loadonden@yahoo.com>
2018-09-10 18:10:29 +0500

    hide rails login ui

--- app/javascript/components/registration.vue
+++ app/javascript/components/registration.vue
@@ -1,39 +1,42 @@
 <template>
 	<div class="reg">
 		<div class="regplace">
-			<el-button size="mini" @click="dialogFormVisible = true">Регистрация</el-button>
+			<div class="warn">
+				<el-button size="mini" @click="dialogFormVisible = true">Регистрация</el-button>
 
-			<el-dialog title="Создать аккаунт:" :visible.sync="dialogFormVisible">
-			  <el-form :model="form">
-			    <el-form-item size="mini" label="Емайл:" :label-width="formLabelWidth">
-			      <el-input v-model="form.email" auto-complete="off"></el-input>
-			    </el-form-item>
-			    <el-form-item size="mini" label="Имя пользователя:" :label-width="formLabelWidth">
-			      <el-input v-model="form.username" auto-complete="off"></el-input>
-			    </el-form-item>
-			    <el-form-item size="mini" label="Пароль:" :label-width="formLabelWidth">
-			      <el-input v-model="form.password" auto-complete="off"></el-input>
-			    </el-form-item>
-			    <el-form-item size="mini" label="Пароль:" :label-width="formLabelWidth">
-			      <el-input v-model="form.password_confirmation" auto-complete="off"></el-input>
-			    </el-form-item>
-<!-- 			    <el-form-item size="mini" label="Пароль еще раз:" :label-width="formLabelWidth">
-			      <el-input v-model="form.password" auto-complete="off"></el-input>
-			    </el-form-item> -->
-			   <!--  <el-form-item label="Zones" :label-width="formLabelWidth">
-			      <el-select v-model="form.region" placeholder="Please select a zone">
-			        <el-option label="Zone No.1" value="shanghai"></el-option>
-			        <el-option label="Zone No.2" value="beijing"></el-option>
-			      </el-select>
-			    </el-form-item> -->
-			 		</el-form>
-			 		<el-button size="mini" @click="dialogFormVisible = false">Отмена</el-button>
-				  <el-button size="mini" type="primary" @click="onSubmit" >Подтвердить</el-button>
-			  	<span slot="footer" class="dialog-footer">
-	
-			  	</span>
+				<el-dialog title="Создать аккаунт:" :visible.sync="dialogFormVisible" >
+				  <el-form :model="form">
+				    <el-form-item size="mini" label="Емайл:" :label-width="formLabelWidth">
+				      <el-input v-model="form.email" auto-complete="off"></el-input>
+				    </el-form-item>
+				    <el-form-item size="mini" label="Имя пользователя:" :label-width="formLabelWidth">
+				      <el-input v-model="form.username" auto-complete="off"></el-input>
+				    </el-form-item>
+				    <el-form-item size="mini" label="Пароль:" :label-width="formLabelWidth">
+				      <el-input v-model="form.password" auto-complete="off"></el-input>
+				    </el-form-item>
+				    <el-form-item size="mini" label="Пароль:" :label-width="formLabelWidth">
+				      <el-input v-model="form.password_confirmation" auto-complete="off"></el-input>
+				    </el-form-item>
+	<!-- 			    <el-form-item size="mini" label="Пароль еще раз:" :label-width="formLabelWidth">
+				      <el-input v-model="form.password" auto-complete="off"></el-input>
+				    </el-form-item> -->
+				   <!--  <el-form-item label="Zones" :label-width="formLabelWidth">
+				      <el-select v-model="form.region" placeholder="Please select a zone">
+				        <el-option label="Zone No.1" value="shanghai"></el-option>
+				        <el-option label="Zone No.2" value="beijing"></el-option>
+				      </el-select>
+				    </el-form-item> -->
+				 		</el-form>
+				 		<el-button size="mini" @click="dialogFormVisible = false">Отмена</el-button>
+					  <el-button size="mini" type="primary" @click="onSubmit" >Подтвердить</el-button>
+				  	<span slot="footer" class="dialog-footer">
+		
+				  	</span>
 
-			</el-dialog>
+				</el-dialog>
+			</div>
+			
 				
 
 		</div>
@@ -86,11 +89,25 @@ export default {
 <style scoped>
 @import "_variables";
 @import "_extends";
+.warn {
+	padding: 4px;
+
+	background: repeating-linear-gradient(
+  45deg,
+  rgba(0, 0, 0, 0.2),
+  rgba(0, 0, 0, 0.2) 10px,
+  rgba(0, 0, 0, 0.3) 10px,
+  rgba(0, 0, 0, 0.3) 20px
+	),
+
+	url(http://s3-us-west-2.amazonaws.com/s.cdpn.io/3/old_map_@2X.png);
+}
 .reg {
 	padding-bottom: 1em;
 	color: $teagreen;
 }
 .regplace {
+	
 	display: flex;
 	justify-content: center;
 } 
