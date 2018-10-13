e80b43b12b1e85ea96229f88c2dc0a38d1bdc7bb
bananvyhe <loadonden@yahoo.com>
2018-10-13 22:47:24 +0500

    rollback db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_13_164654) do
+ActiveRecord::Schema.define(version: 2018_10_12_084218) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,14 +39,6 @@ ActiveRecord::Schema.define(version: 2018_10_13_164654) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["list_id"], name: "index_cards_on_list_id"
-  end
-
-  create_table "clients", force: :cascade do |t|
-    t.integer "cash"
-    t.bigint "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_clients_on_user_id"
   end
 
   create_table "lists", force: :cascade do |t|
@@ -103,6 +95,5 @@ ActiveRecord::Schema.define(version: 2018_10_13_164654) do
   end
 
   add_foreign_key "cards", "lists"
-  add_foreign_key "clients", "users"
   add_foreign_key "recordings", "locations"
 end
