eeddb5d18267b1eb537e74f4a9dc555a926f1262
bananvyhe <loadonden@yahoo.com>
2018-11-17 23:49:02 +0500

    1

--- app/controllers/api/v1/authentication_controller.rb
+++ app/controllers/api/v1/authentication_controller.rb
@@ -5,7 +5,8 @@ class Api::V1::AuthenticationController < ApiController
     user = User.find_by(email: params[:user][:email])
     if user
       if user.valid_password? params[:user][:password]
-        render json: { token: JsonWebToken.encode(sub: user.id, role: user.role, username: user.username ) 
+        render json: { token: JsonWebToken.encode(sub: user.id, role: user.role, username: user.username ) ,
+         refreshToken: JsonWebToken.refr(id: user.id) 
         }
         sign_in user
       else
