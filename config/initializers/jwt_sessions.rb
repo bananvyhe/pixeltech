a032b99ca756103ef2add6530bb73446cac28219
bananvyhe <loadonden@yahoo.com>
2018-11-29 16:39:03 +0500

    fix

--- config/initializers/jwt_sessions.rb
+++ config/initializers/jwt_sessions.rb
@@ -1,5 +1,5 @@
 JWTSessions.algorithm = 'HS256'
 JWTSessions.encryption_key = Rails.application.credentials.secret_key_base 
 #JWTSessions.refresh_exp_time = 604800 # 1 week in seconds
-JWTSessions.access_exp_time = 1800
+JWTSessions.access_exp_time = 300
 JWTSessions.refresh_exp_time = 1209600
