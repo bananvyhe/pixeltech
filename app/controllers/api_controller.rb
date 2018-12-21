3380b70114222294db3f1eb44cd87afd9beca409
bananvyhe <loadonden@yahoo.com>
2018-12-21 10:13:22 +0500

    test

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,7 +1,7 @@
 class ApiController < ApplicationController
 	skip_before_action :verify_authenticity_token
   before_action :authenticate_user!
-  before_action :set_default_format
+  #before_action :set_default_format
 
   private
 
