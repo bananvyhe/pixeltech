a4180ebad8a3dd6519edf4ff88a3eb2562fde61a
bananvyhe <loadonden@yahoo.com>
2018-12-27 17:02:25 +0500

    2

--- app/controllers/api/v1/inboxes_controller.rb
+++ app/controllers/api/v1/inboxes_controller.rb
@@ -34,7 +34,7 @@ class Api::V1::InboxesController < ApiController
     
     if @check == @inbox.sha1_hash 
       @inbox.save
-      @fee =  Client.find(params[:label])
+      @fee =  Client.find_by_user_id(params[:label])
       @fee.ballance += @inbox.withdraw_amount
       @fee.save
     end 
