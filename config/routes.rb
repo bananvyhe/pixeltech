c541c0fca2e30e0afaf32001478e182031c3bfbf
bananvyhe <loadonden@yahoo.com>
2019-01-05 21:47:36 +0500

    login setup for sidekiq

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,12 @@ Rails.application.routes.draw do
     # - See https://thisdata.com/blog/timing-attacks-against-string-comparison/
     # - Use & (do not use &&) so that it doesn't short circuit.
     # - Use digests to stop length information leaking (see also ActiveSupport::SecurityUtils.variable_size_secure_compare)
-    ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(username), ::Digest::SHA256.hexdigest(ENV["SIDEKIQ_USERNAME"])) &
-      ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(password), ::Digest::SHA256.hexdigest(ENV["SIDEKIQ_PASSWORD"]))
+
+    login_hash = ::Digest::SHA256.hexdigest(username)
+    password_hash = ::Digest::SHA256.hexdigest(password)
+
+    ActiveSupport::SecurityUtils.secure_compare(login_hash, ::Digest::SHA256.hexdigest(Rails.application.credentials.username)) &
+      ActiveSupport::SecurityUtils.secure_compare(password_hash, ::Digest::SHA256.hexdigest(Rails.application.credentials.password))
   end # if Rails.env.production?
   mount Sidekiq::Web, at: "/sidekiq"
   
