78f0c524d74e939007defa5c24db547e1ebeb031
bananvyhe <loadonden@yahoo.com>
2019-01-08 10:54:32 +0500

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,7 @@
 require "sidekiq/web"
 
 Rails.application.routes.draw do
-  Sidekiq::Web.use Rack::Auth::Basic do |username, password|
-    # Protect against timing attacks:
-    # - See https://codahale.com/a-lesson-in-timing-attacks/
-    # - See https://thisdata.com/blog/timing-attacks-against-string-comparison/
-    # - Use & (do not use &&) so that it doesn't short circuit.
-    # - Use digests to stop length information leaking (see also ActiveSupport::SecurityUtils.variable_size_secure_compare)
-
-    login_hash = ::Digest::SHA256.hexdigest(username)
-    password_hash = ::Digest::SHA256.hexdigest(password)
-
-    ActiveSupport::SecurityUtils.secure_compare(login_hash, ::Digest::SHA256.hexdigest(Rails.application.credentials.username)) &
-      ActiveSupport::SecurityUtils.secure_compare(password_hash, ::Digest::SHA256.hexdigest(Rails.application.credentials.password))
-  end # if Rails.env.production?
-  mount Sidekiq::Web, at: "/sidekiq"
+  
   
   resources :inboxes
   resources :siteowners
@@ -70,4 +57,18 @@ Rails.application.routes.draw do
  
   match "/404", :to => "errors#not_found", :via => :all
 	match "/500", :to => "errors#internal_server_error", :via => :all
+  Sidekiq::Web.use Rack::Auth::Basic do |username, password|
+    # Protect against timing attacks:
+    # - See https://codahale.com/a-lesson-in-timing-attacks/
+    # - See https://thisdata.com/blog/timing-attacks-against-string-comparison/
+    # - Use & (do not use &&) so that it doesn't short circuit.
+    # - Use digests to stop length information leaking (see also ActiveSupport::SecurityUtils.variable_size_secure_compare)
+
+    login_hash = ::Digest::SHA256.hexdigest(username)
+    password_hash = ::Digest::SHA256.hexdigest(password)
+
+    ActiveSupport::SecurityUtils.secure_compare(login_hash, ::Digest::SHA256.hexdigest(Rails.application.credentials.username)) &
+      ActiveSupport::SecurityUtils.secure_compare(password_hash, ::Digest::SHA256.hexdigest(Rails.application.credentials.password))
+  end # if Rails.env.production?
+  mount Sidekiq::Web, at: "/sidekiq"
 end
