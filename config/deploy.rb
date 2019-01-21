565a03d1badcfd38f42074832ed06ed26bda53f9
bananvyhe <loadonden@yahoo.com>
2019-01-21 20:29:02 +0500

    2

--- config/deploy.rb
+++ config/deploy.rb
@@ -36,40 +36,41 @@ set :pty,  false
 set :init_system, :upstart
 set :sidekiq_monit_use_sudo, false
 set :upstart_service_name, 'sidekiq'
-namespace :sidekiq do
-  task :quiet do
-    on roles(:app) do
-      puts capture("pgrep -f 'sidekiq' | xargs kill -TSTP") 
-    end
-  end
-  task :restart do
-    on roles(:app) do
-      execute :sudo, :initctl, :restart, :workers
-    end
-  end
-end
 
-after 'deploy:starting', 'sidekiq:quiet'
-after 'deploy:reverted', 'sidekiq:restart'
-after 'deploy:published', 'sidekiq:restart'
+# namespace :sidekiq do
+#   task :quiet do
+#     on roles(:app) do
+#       puts capture("pgrep -f 'sidekiq' | xargs kill -TSTP") 
+#     end
+#   end
+#   task :restart do
+#     on roles(:app) do
+#       execute :sudo, :initctl, :restart, :workers
+#     end
+#   end
+# end
 
-# If you wish to use Inspeqtor to monitor Sidekiq
-# https://github.com/mperham/inspeqtor/wiki/Deployments
-namespace :inspeqtor do
-  task :start do
-    on roles(:app) do
-      execute :inspeqtorctl, :start, :deploy
-    end
-  end
-  task :finish do
-    on roles(:app) do
-      execute :inspeqtorctl, :finish, :deploy
-    end
-  end
-end
+# after 'deploy:starting', 'sidekiq:quiet'
+# after 'deploy:reverted', 'sidekiq:restart'
+# after 'deploy:published', 'sidekiq:restart'
 
-before 'deploy:starting', 'inspeqtor:start'
-after 'deploy:finished', 'inspeqtor:finish'
+# # If you wish to use Inspeqtor to monitor Sidekiq
+# # https://github.com/mperham/inspeqtor/wiki/Deployments
+# namespace :inspeqtor do
+#   task :start do
+#     on roles(:app) do
+#       execute :inspeqtorctl, :start, :deploy
+#     end
+#   end
+#   task :finish do
+#     on roles(:app) do
+#       execute :inspeqtorctl, :finish, :deploy
+#     end
+#   end
+# end
+
+# before 'deploy:starting', 'inspeqtor:start'
+# after 'deploy:finished', 'inspeqtor:finish'
 #set :upstart_service_name, 'sidekiq'
 #set :pty,  false
 
