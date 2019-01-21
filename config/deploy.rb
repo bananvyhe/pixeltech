2bc8002a5228801eda49d112da0f80acc4254878
bananvyhe <loadonden@yahoo.com>
2019-01-21 19:54:24 +0500

    2

--- config/deploy.rb
+++ config/deploy.rb
@@ -33,7 +33,7 @@ set :repo_url, "git@github.com:bananvyhe/pixeltech.git"
 set :pty,  false
 set :init_system, :systemd
 set :sidekiq_monit_use_sudo, false
-set :upstart_service_name, 'sidekiq'
+set :upstart_service_name, 'sidekiq-production.service'
 #set :pty,  false
 SSHKit.config.command_map[:sidekiq] = "bundle exec sidekiq"
 SSHKit.config.command_map[:sidekiqctl] = "bundle exec sidekiqctl"
