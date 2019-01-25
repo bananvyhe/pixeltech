3a2d9e3a4e880b77f2b062f9b5d228692d516262
bananvyhe <loadonden@yahoo.com>
2019-01-25 17:32:20 +0500

    was ballance from CashService services

--- app/workers/cash_worker.rb
+++ app/workers/cash_worker.rb
@@ -2,7 +2,8 @@ class CashWorker
 	include Sidekiq::Worker
 
 	def perform
-		 Client.find_each do |c| 
+		client = Client.all
+		 client.find_each do |c| 
 		 	c.ballance -= 1.to_i
 		 	c.save
 		 end
