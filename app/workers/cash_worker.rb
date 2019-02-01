ee7a7a4478a32e58ac8a634d6d6312449ecb8d24
bananvyhe <loadonden@yahoo.com>
2019-02-01 10:32:23 +0500

    step vars glob

--- app/workers/cash_worker.rb
+++ app/workers/cash_worker.rb
@@ -2,10 +2,10 @@ class CashWorker
 	include Sidekiq::Worker
 
 	def perform
-		
+		step = $step
 		client = Client.all
 		client.find_each do |c| 
-		 	c.ballance -= $step
+		 	c.ballance -= step
 		 	c.save
 		end
 	end
