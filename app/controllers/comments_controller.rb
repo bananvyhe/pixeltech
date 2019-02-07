9cd4b60d6886d94690424a9ccf9e37d1b8d1d946
bananvyhe <loadonden@yahoo.com>
2019-02-07 14:17:11 +0500

    main structure

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,13 @@
 class CommentsController < ApplicationController	
-	before_action :authenticate_user!
-
+	#before_action :authenticate_user!
+  #before_action :authorize_access_request!
 	def create
 		@comment = @commentable.comments.new(comment_params)
     @comment.user = current_user
 		if @comment.save
 			 respond_to do |format|
         format.html { redirect_to @commentable }
-        format.js
+        # format.js
       end
     else
       redirect_to @commentable, alert: "Something went wrong"
--- app/controllers/posts/comments_controller.rb
+++ app/controllers/posts/comments_controller.rb
@@ -1,9 +1,7 @@
 class Posts::CommentsController < CommentsController 
 	before_action :set_commentable
 
-	def create
-		
-	end
+ 
 	private
 	def set_commentable
 		@commentable = Post.find(params[:post_id])
