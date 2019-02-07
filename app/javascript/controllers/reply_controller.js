8ed402a78e64a9c397c5bff62f2333b9f968f07e
bananvyhe <loadonden@yahoo.com>
2019-02-07 22:27:35 +0500

    comments setup

--- app/javascript/controllers/reply_controller.js
+++ app/javascript/controllers/reply_controller.js
@@ -10,9 +10,13 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-  static targets = [ "output" ]
+  static targets = [ "form" ]
 
-  connect() {
-    this.outputTarget.textContent = 'Hello, Stimulus!'
+  // connect() {
+  //   this.outputTarget.textContent = 'Hello, Stimulus!'
+  // }
+  toggle(event) {
+  	event.preventDefault()
+  	this.formTarget.classList.toggle("d-none")
   }
 }
