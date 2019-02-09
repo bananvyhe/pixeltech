a18bf7d437d50a16005a24ae0be6b9c71dfbec58
bananvyhe <loadonden@yahoo.com>
2019-02-09 21:33:43 +0500

    1

--- app/javascript/head.vue
+++ app/javascript/head.vue
@@ -1,8 +1,10 @@
 мос<!-- :class="{ bgclass: activatorclass }" -->
 <template>
 	<div class="firstpagebg">
+
     <div  id="bg " :style="{'background-image': 'url('+require('./images/scheme.svg')+'?id='+Math.random()+')'}" class="bgan " :class="{ bgclass: activatorclass }">
-       
+     
+
       <div class="formsZone">
       
         <div v-if="$store.getters.token != null" >
