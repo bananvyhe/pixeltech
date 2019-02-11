d06a922d3e27e73b04276f0c9d3e5fd72ec8d1bb
bananvyhe <loadonden@yahoo.com>
2019-02-11 22:02:44 +0500

    destroy comment implement

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -12,8 +12,12 @@ class CommentsController < ApplicationController
     else
       redirect_to @commentable, alert: "Something went wrong"
     end
-		 
 	end
+  def destroy
+    @comment = @commentable.comments.find(params[:id])
+    @comment.destroy
+    redirect_to @commentable
+  end
 	private
  		def comment_params
       params.require(:comment).permit(:body, :parent_id)
