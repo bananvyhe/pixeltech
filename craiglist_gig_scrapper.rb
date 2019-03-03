5346383b2a9ec9fcfe7a29535ae010a99562174c
bananvyhe <loadonden@yahoo.com>
2019-03-03 23:27:55 +0500

    scrapping test

--- craiglist_gig_scrapper.rb
+++ craiglist_gig_scrapper.rb
@@ -1,1 +1,14 @@
-puts "i am here"
+require 'open-uri'
+require 'nokogiri'
+
+url="https://vk.com/powermetalheads"
+
+document = open(url)
+content = document.read
+parsed_content = Nokogiri::HTML(content)
+
+parsed_content.css('.pi_text').each do |row|
+	title = row.inner_text
+	puts title
+	puts '--------------------------'
+end
