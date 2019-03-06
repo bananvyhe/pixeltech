779a6482f80780343815e844ea961fa3d0ad132b
bananvyhe <loadonden@yahoo.com>
2019-03-06 15:38:19 +0500

    parsing deep on page

--- craiglist_gig_scrapper.rb
+++ craiglist_gig_scrapper.rb
@@ -7,17 +7,38 @@ agent = Mechanize.new
 url="https://vk.com/powermetalheads"
 
 page = agent.get(url)
-puts page.inspect
+# puts page.inspect
+# print page.body
+# puts "#{page.links}"
 
 # document = open(url)
 # content = document.read
 # parsed_content = Nokogiri::HTML(content)
+show_more = agent.page.link_with(text: 'Show more').click
+.link_with(text: 'Show more').click
+.link_with(text: 'Show more').click
+.link_with(text: 'Show more').click
+.link_with(text: 'Show more').click
+ 
+ 
+page.css('.wall_item').each do |row|
+	title     = row.css('.pi_text').inner_text
+	posted_at = row.css('.wi_date').inner_text
 
-# parsed_content.css('.wall_item').each do |row|
-# 	title     = row.css('.pi_text').inner_text
-# 	posted_at = row.css('.wi_date').inner_text
+	puts title
+	puts posted_at
+	puts '--------------------------'
+end
 
-# 	puts title
-# 	puts posted_at
-# 	puts '--------------------------'
-# end
+show_more.css('.wall_item').each do |row|
+	title     = row.css('.pi_text').inner_text
+	posted_at = row.css('.wi_date').inner_text
+
+	puts title
+	puts posted_at
+	puts '--------------------------'
+end
+
+
+
+
