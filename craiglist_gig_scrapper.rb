e1aa99c3a69511804fe385b3f919f6ad1939339b
bananvyhe <loadonden@yahoo.com>
2019-03-07 18:47:01 +0500

    to json result in array

--- craiglist_gig_scrapper.rb
+++ craiglist_gig_scrapper.rb
@@ -1,6 +1,8 @@
 # require 'open-uri'
 # require 'nokogiri'
 require 'mechanize'
+require 'json'
+require 'Pry'
 
 agent = Mechanize.new
 
@@ -20,6 +22,7 @@ show_more = agent.page.link_with(text: 'Show more').click
 # .link_with(text: 'Show more').click
 # .link_with(text: 'Show more').click
  # print show_more.body 
+ @rowsd = Array.new
 def selection_scrapped(row)
 	title     = row.css('.pi_text').inner_text
 	posted_at = row.css('.wi_date').inner_text
@@ -28,14 +31,24 @@ def selection_scrapped(row)
 	## регулярные выражения здесь не работают, необходщим синтаксис нокогири - реализован экстракт ссылок с картинками из стилей
  	# (/https.*jpg/)
 	thumb_map_img_as_div = row.search('.thumb_map_img_as_div').map{ |n| n['style'][/url\((.+)\)/, 1] }
+  
+	# puts thumb_map_img_as_div
+	# puts title
+	# puts posted_at
+	# puts v_like	
+	# puts v_views
+	data = {
+    title: title,
+    posted_at: posted_at,
+    v_views: v_views,
+    v_like: v_like,
+    thumb_map_img_as_div: thumb_map_img_as_div
+  }	
 
-	puts thumb_map_img_as_div
-	puts title
-	puts posted_at
-	puts v_like	
-	puts v_views
+  @rowsd << data
+	# Pry.start(binding)
 
-	puts '--------------------------'
+	# puts '--------------------------'
 end
 
 page.css('.wall_item').each do |row|
@@ -45,7 +58,7 @@ end
 show_more.css('.wall_item').each do |row|
 	selection_scrapped(row)
 end
-
+puts puts JSON.pretty_generate(@rowsd)
 
 
 
