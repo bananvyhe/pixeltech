62e12fc82ef574881185f1c03ac6df35e7e2554f
bananvyhe <loadonden@yahoo.com>
2019-03-09 09:45:38 +0500

    make check out exists post by wall field

--- app/controllers/api/v1/vks_controller.rb
+++ app/controllers/api/v1/vks_controller.rb
@@ -19,9 +19,15 @@ class Api::V1::VksController < ApiController
   # POST /Vks.json
   def create
   	params.require(:_json).each do |d|
-			@vk = Vk.new({:wall => d[:wall],:title => d[:title],:posted_at => d[:posted_at],:v_views => d[:v_views], :v_like => d[:v_like], :thumb_map_img_as_div => d[:thumb_map_img_as_div]})
-	    @vk.save
+  		wall = Vk.find_by(wall: d[:wall])
+  		if wall.present?
+				@vk = Vk.new({:wall => d[:wall],:title => d[:title],:posted_at => d[:posted_at],:v_views => d[:v_views], :v_like => d[:v_like], :thumb_map_img_as_div => d[:thumb_map_img_as_div]})
+		    @vk.save
+	  	end
   	end
   end 
+
+      
+ 
  
 end
