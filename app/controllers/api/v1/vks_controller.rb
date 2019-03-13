f20e6dda50eedb2462f5c1da0c752c5c08e55fc0
bananvyhe <loadonden@yahoo.com>
2019-03-13 19:07:10 +0500

    async transact to bd with sidekiq

--- app/controllers/api/v1/vks_controller.rb
+++ app/controllers/api/v1/vks_controller.rb
@@ -19,23 +19,20 @@ class Api::V1::VksController < ApiController
   # POST /Vks
   # POST /Vks.json
   def create
+
 		params.require(:_json).each do |d|
 			@vk = Vk.new({:title => d[:title],:posted_at => d[:posted_at],:v_views => d[:v_views], :v_like => d[:v_like], :thumb_map_img_as_div => d[:thumb_map_img_as_div],:wall => d[:wall]})
   	 	like = d[:v_like]
   	 	views = d[:v_views]
-  	 	posted_at = d[:posted_at] 
- 			raiting = like.to_f/views.to_f*1000
+  	 	posted_at = d[:posted_at]
+  	 	wald = d[:wall] 
+ 			
  			# binding.pry
-  	 	wald = d[:wall]
-  	 	if df = Vk.find_by(:wall => wald.to_s)
-  	 		df.update_attribute(:raiting, raiting.round(2))
-  			df.update_attribute(:v_views, views)
-  			df.update_attribute(:v_like, like)
-  			df.update_attribute(:posted_at, posted_at)
-			end
+
   		if !Vk.find_by(:wall => wald.to_s)
   			@vk.save	
 		  end
+  	 	TobdWorker.perform_async(like, views, posted_at, wald)
   	end
   end 
 end
---
+++ app/workers/tobd_worker.rb
@@ -0,0 +1,14 @@
+class TobdWorker
+  include Sidekiq::Worker
+
+  def perform (like, views, posted_at, wald)
+  	raiting = like.to_f/views.to_f*1000
+	    if df = Vk.find_by(:wall => wald.to_s)
+	  	 		df.update_attribute(:raiting, raiting.round(2))
+	  			df.update_attribute(:v_views, views)
+	  			df.update_attribute(:v_like, like)
+	  			df.update_attribute(:posted_at, posted_at)
+			end
+  	
+  end
+end
