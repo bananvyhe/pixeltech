54bcf6acf5135d0992e3939f8cb7e75b0dc59a8d
bananvyhe <loadonden@yahoo.com>
2019-03-13 18:03:39 +0500

    make raiting

--- app/controllers/api/v1/vks_controller.rb
+++ app/controllers/api/v1/vks_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::VksController < ApiController
- 
+ # require 'pry'
 	skip_before_action :authenticate_user!
 
 	def index
@@ -23,12 +23,15 @@ class Api::V1::VksController < ApiController
 			@vk = Vk.new({:title => d[:title],:posted_at => d[:posted_at],:v_views => d[:v_views], :v_like => d[:v_like], :thumb_map_img_as_div => d[:thumb_map_img_as_div],:wall => d[:wall]})
   	 	like = d[:v_like]
   	 	views = d[:v_views]
- 			# raiting = like.to_f/views.to_f
+  	 	posted_at = d[:posted_at] 
+ 			raiting = like.to_f/views.to_f*1000
+ 			# binding.pry
   	 	wald = d[:wall]
   	 	if df = Vk.find_by(:wall => wald.to_s)
-  	 		# df.update_attribute(:raiting, raiting.round(1))
+  	 		df.update_attribute(:raiting, raiting.round(2))
   			df.update_attribute(:v_views, views)
   			df.update_attribute(:v_like, like)
+  			df.update_attribute(:posted_at, posted_at)
 			end
   		if !Vk.find_by(:wall => wald.to_s)
   			@vk.save	
