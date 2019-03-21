3ee6aaa2a38cb45e0663d32473c25eca20b3041e
bananvyhe <loadonden@yahoo.com>
2019-03-21 22:11:06 +0500

    fix

--- app/controllers/api/v1/vks_controller.rb
+++ app/controllers/api/v1/vks_controller.rb
@@ -29,10 +29,10 @@ class Api::V1::VksController < ApiController
   	 	posted_at = d[:posted_at]
   	 	wald = d[:wall] 
       medias_row = d[:medias_row]
-      thumb_map_img_as_div = d[:thumb_map_img_as_div]
+      thumb_map_img_as_div = d[:medias_row]
  			# binding.pry
 
-  		if !Vk.find_by(:wall => wald.to_s)
+  		if !Vk.find_by(:wall => wald)
   			@vk.save	
 		  end
   	 	TobdWorker.perform_async(like, views, posted_at)
--- app/workers/tobd_worker.rb
+++ app/workers/tobd_worker.rb
@@ -4,17 +4,15 @@ class TobdWorker
   def perform (like, views, posted_at)
   	raiting = like.to_f/views.to_f*1000
 
-	  	 		df.update_attribute(:raiting, raiting.round(2))
-	  			df.update_attribute(:v_views, views)
-	  			df.update_attribute(:v_like, like)
-	  			df.update_attribute(:posted_at, posted_at)
- 
+ 		df.update_attribute(:raiting, raiting.round(2))
+		df.update_attribute(:v_views, views)
+		df.update_attribute(:v_like, like)
+		df.update_attribute(:posted_at, posted_at)
 
-	  			# if df[:created_at] > Date.today - 7.days
-	  			# 	# df.update_attribute(:raiting, -20)
-	  			# end
 
-			end
-  	
-  end
+		# if df[:created_at] > Date.today - 7.days
+		# 	# df.update_attribute(:raiting, -20)
+		# end
+
+	end
 end
