aa55a55477dd0858f255808f859dd9f4b58a80f6
bananvyhe <loadonden@yahoo.com>
2019-03-21 23:15:25 +0500

    fix

--- app/controllers/landing_page_controller.rb
+++ app/controllers/landing_page_controller.rb
@@ -2,22 +2,22 @@ class LandingPageController < ApplicationController
 	def index
 		if (current_user)
 	    path = case current_user.role
-	    	# when 'superadmin'
-	     #    users_path
-	      # when 'admin'
-	      #   users_path
-	      # when 'user'
-	      #   posts_path
-	      # when 'voodoo'
-	      #   # voodoo_index_path
-	      #   posts_path
+	    	when 'superadmin'
+	        users_path
+	      when 'admin'
+	        users_path
+	      when 'user'
+	        posts_path
+	      when 'voodoo'
+	        # voodoo_index_path
+	        posts_path
 	      when 'client'
 	        siteowners_path
 	      else
 	      
 	      	# If you want to raise an exception or have a default root for users without roles
 	    end
-	    if current_user.role != "user"
+	    if current_user.role != "user" &&  current_user.role != "voodoo"
 	    	redirect_to path  
 	    end 
 	  else
