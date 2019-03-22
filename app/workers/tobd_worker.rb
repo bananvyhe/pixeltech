d0bf83489ebc14ada068fb85a29b398d43140371
bananvyhe <loadonden@yahoo.com>
2019-03-22 08:38:59 +0500

    w

--- app/workers/tobd_worker.rb
+++ app/workers/tobd_worker.rb
@@ -3,12 +3,12 @@ class TobdWorker
 
   def perform (like, views, posted_at)
   	raiting = like.to_f/views.to_f*1000
-
+	if df = Vk.find_by(:v_like => v_like.to_s)
  		df.update_attribute(:raiting, raiting.round(2))
 		df.update_attribute(:v_views, views)
 		df.update_attribute(:v_like, like)
 		df.update_attribute(:posted_at, posted_at)
-
+	end
 
 		# if df[:created_at] > Date.today - 7.days
 		# 	# df.update_attribute(:raiting, -20)
