e76e5f98d801652d2ca921ea02b69bbdaf3df2f2
bananvyhe <loadonden@yahoo.com>
2019-03-25 23:22:24 +0500

    fix

--- app/controllers/api/v1/vks_controller.rb
+++ app/controllers/api/v1/vks_controller.rb
@@ -21,19 +21,15 @@ class Api::V1::VksController < ApiController
   # POST /Vks.json
   def create
 		params.require(:_json).each do |d|
-			@@vk = Vk.new({:title => d[:title],:medias_row => d[:medias_row],:posted_at => d[:posted_at],:v_views => d[:v_views], :v_like => d[:v_like], :thumb_map_img_as_div => d[:thumb_map_img_as_div],:wall => d[:wall]})
-  	 	like = d[:v_like]
-  	 	views = d[:v_views]
-  	 	posted_at = d[:posted_at]
-  	 	wald = d[:wall] 
-      medias_row = d[:medias_row]
-      thumb_map_img_as_div = d[:medias_row]
-      title = d[:title]
- 			# binding.pry
-      # print Vk.find_by_wall(wald)
-      
-        TobdWorker.perform_async(like, views, posted_at, wald, title)
-		  end
+    wall = d[:wall]
+    ike = d[:v_like]
+    views = d[:v_views]
+    posted_at = d[:posted_at]
+    medias_row = d[:medias_row]
+    thumb_map_img_as_div = d[:thumb_map_img_as_div]
+    title = d[:title]
+    TobdWorker.perform_async(wall, like, views, posted_at, medias_row, thumb_map_img_as_div, title)
+	end
 
   end 
   private
--- app/workers/tobd_worker.rb
+++ app/workers/tobd_worker.rb
@@ -1,23 +1,18 @@
-class TobdWorker
+class TobdWorker < ApplicationController
   include Sidekiq::Worker
+ 
+  def perform (wall, like, views, posted_at, medias_row, thumb_map_img_as_div, title)
+ 		raiting = like.to_f/views.to_f*1000	
+  	@vk = Vk.new({:title => title,:medias_row => medias_row,:posted_at => posted_at,:v_views => views, :v_like => like, :thumb_map_img_as_div => thumb_map_img_as_div, :wall => wall})
+		df = Vk.find_by(wall: wall.to_s)
 
-  def perform (like, views, posted_at, wald, title)
-  	raiting = like.to_f/views.to_f*1000
-	# if df = Vk.find_by(:wall => wald.to_s)
- 	
-	# end
-		df = Vk.find_by(wall: wald.to_s)
   	if (df == nil)
-  			@@vk.save
+  		@vk.save
     else
 			df.update_attribute(:raiting, raiting.round(2))
 			df.update_attribute(:v_views, views)
 			df.update_attribute(:v_like, like)
 			df.update_attribute(:posted_at, posted_at)
-		# if df[:created_at] > Date.today - 7.days
-		# 	# df.update_attribute(:raiting, -20)
-		# end
-
 		end
 	end
 end
