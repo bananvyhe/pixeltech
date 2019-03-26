72cdeeabb941198233e322ea1367e4a2413cb7dd
bananvyhe <loadonden@yahoo.com>
2019-03-26 23:41:58 +0500

    2

--- app/controllers/api/v1/authentication_controller.rb
+++ app/controllers/api/v1/authentication_controller.rb
@@ -1,5 +1,5 @@
-class Api::V1::AuthenticationController < ApiController
-  skip_before_action :authenticate_user!
+class Api::V1::AuthenticationController < Api2Controller
+ 
   def create
     user = User.find_by(email: params[:user][:email])
     if user
