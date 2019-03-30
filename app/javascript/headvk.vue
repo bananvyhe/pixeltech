39dd8cc5a89b39dc8a55b5a0af787331c24eda2c
bananvyhe <loadonden@yahoo.com>
2019-03-30 15:34:05 +0500

    but onmouse reaction

--- app/javascript/headvk.vue
+++ app/javascript/headvk.vue
@@ -2,7 +2,7 @@
 <template>
 <div id="app">
   <div v-if="alldata.length === 0" class="loading">Загрузка...</div>
-  <div v-for="data in alldata" class="vkpost">
+  <div v-for="data in alldata" class="vkpost" >
     <div class="itembg2" :style="{height: carouselh}" v-if="data.thumb_map_img_as_div.split(',').length > 1">
       <el-carousel  type="card" :height="carouselh" >
         <el-carousel-item v-for="item in data.thumb_map_img_as_div.split(',')" :key="item">
@@ -25,9 +25,10 @@
         <div class="vkraiting largertext">{{data.raiting}}</div>        
       </div>
       <div v-if="data.medias_row" class="mediabutton">
-        <a target="_blank" v-bind:href='"https://vk.com"+data.wall.slice(2, -2)'><div class="link">
-          
-        </div></a>
+<!--         <a target="_blank" v-on:click.capture="linkswicher=true" v-bind:href='""+data.wall.slice(2, -2)'><div class="link" :class="[linkswicher ? linkactive : '', link]"> -->
+       <!--  <component v-bind:href='""+data.wall.slice(2, -2)'></component> -->
+         <energy-button v-bind:href='""+data.wall.slice(2, -2)'></energy-button> 
+      <!-- </a> -->
       </div>
  
     </div>
@@ -37,11 +38,45 @@
 
 <script>
 import axios from 'axios'
+var cmp = {
+
+  data: function(){
+    return {
+      status: 'link'
+    };
+  },
+  template: '<a @click="changeStatus" @mouseover="overAction" @mouseleave="leaveAction" class="energy" :class="status" target="_blank" ></a>',
+
+  methods: {
+    overAction: function() {
+      this.status = 'linkactive';
+    },
+    leaveAction: function() {
+      this.status = 'link';
+    },
+    changeStatus: function() {
+            this.status = 'link';
+   
+
+       //  if ($('.energy').hasClass('linkactive'))
+       // {
+       //   this.status = 'linkactive2';
+       // } else
+       // {
+       //  this.status = 'linkactive';
+       // }
+       
+    }
+  }
+};
  
 export default {
  
   data: function () {
     return {  
+      linkswicher: false,
+      linkactive: 'linkactive',
+      link: 'link',
       bganim: {
         backgroundPosition: "center",
       },
@@ -54,6 +89,15 @@ export default {
       bottom: false,
       beers: [] 
     }
+  },
+  components: {
+    'energy-button': cmp
+    // 'lk': {
+    //   template: '<a  v-bind:is="view" v-on:click ="view = lka" target="_blank" class="link"></a>'
+    // },
+    // 'lka': {
+    //   template: '<a target="_blank" class="linkactive"></a>'
+    // }
   },
   watch: {
     bottom(bottom) {
@@ -126,47 +170,76 @@ export default {
 @import "_variables";
 @import "_extends";
 .link {
- content: 'sdegsdg';
-/*  padding: 24px 13px;*/
+  content: '';
   display: block;
   position: relative;
-  width: 130px;
-  height: 50px;
-
-  /*display: flex;*/
-/*  justify-content: center;
-  align-content: center;
-  align-items: center;*/
+  width: 150px;
+  height: 70px;
 }
+
 .link:before {
   adjust-font-size: fs b;
   color: color( $str5  blackness(12%));
- /* padding: 20px 15px;*/
   content: 'медиа в окне';
   position: absolute; 
   top: 6px; right: 0;
   bottom: 0; left: 10px; z-index: -1;
 }
+ /*content: url('./images/energy_32.gif');*/
 .link:after {
-/*  z-index: 4000;*/
-  content: url('./images/energy_32.png');
+  /*content: url('./images/energy_32.png');*/
+  content: url('./images/energy29.gif');
   position: absolute; 
-/*  width: 130px;
-  height: 50px;*/
-  top: -17px; right: 0;
-  bottom: 0; left: -17px;
-/*  background: url('./images/energy_32.png');*/
-  /*background-color: #ada;*/
-/*  background-repeat: no-repeat;
-  background-position: center;
-  background-position: -15px 43%;*/
+  top: -18px; right: 0;
+  bottom: 0; left: -19px;
+}
+
+.linkactive {
+  content: '';
+  display: block;
+  position: relative;
+  width: 130px;
+  height: 50px;
+
+}
+.linkactive:before {
+  adjust-font-size: fs b;
+  color: color( $str5  blackness(12%));
+  content: 'медиа в окне';
+  position: absolute; 
+  top: 6px; right: 0;
+  bottom: 0; left: 10px; z-index: -1;
+}
+.linkactive:after {
+  content: url('./images/energy32.gif');
+  position: absolute; 
+  top: -20px; right: 0;
+  bottom: 0; left: -14px;
+}
+.linkactive2 {
+  content: '';
+  display: block;
+  position: relative;
+  width: 130px;
+  height: 50px;
+
+}
+.linkactive2:before {
+  adjust-font-size: fs b;
+  color: color( $str5  blackness(12%));
+  content: 'медиа в окне';
+  position: absolute; 
+  top: 6px; right: 0;
+  bottom: 0; left: 10px; z-index: -1;
+}
+.linkactive2:after {
+  content: url('./images/energy_27_2.gif');
+  position: absolute; 
+  top: -13px; right: 0;
+  bottom: 0; left: -14px;
 }
 .bottom {
   position: relative;
-/*   display: flex;
-  justify-content: center;
-  align-content: center;
-  align-items: center;*/
 }
 #app {
   @media (--only-1600more-screen) {
