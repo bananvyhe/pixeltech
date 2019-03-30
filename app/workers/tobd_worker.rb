f490e6a9273befe12679bdad66b282cb78cb6ef4
bananvyhe <loadonden@yahoo.com>
2019-03-30 21:43:37 +0500

    fix sidekiq

--- app/workers/tobd_worker.rb
+++ app/workers/tobd_worker.rb
@@ -1,7 +1,7 @@
 class TobdWorker < ApplicationController
   include Sidekiq::Worker
  
-  def perform (wall, like, views, posted_at, thumb_map_img_as_div, title)
+  def perform (wall, like, views, posted_at, thumb_map_img_as_div, title, medias_row)
  		raiting = like.to_f/views.to_f*1000	
   	@vk = Vk.new({:title => title,:medias_row => medias_row,:posted_at => posted_at,:v_views => views, :v_like => like, :thumb_map_img_as_div => thumb_map_img_as_div, :wall => wall})
 		df = Vk.find_by(wall: wall.to_s)
