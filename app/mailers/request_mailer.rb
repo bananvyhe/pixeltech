cd4083bfa25b16988cf0d2ff5dc8edc8caf29efd
bananvyhe <loadonden@yahoo.com>
2019-04-21 15:43:36 +0500

    anim mail button

--- app/mailers/request_mailer.rb
+++ app/mailers/request_mailer.rb
@@ -2,6 +2,6 @@ class RequestMailer < ApplicationMailer
 	def message_request_mail(mes, email) 
 		@mes = mes
 		@email = email
-		mail(to:'loadonden@yahoo.com',from:"admin@pixeltech.ru",subject:'заявка')
+		mail(to:'loadonden@yahoo.com',from:"admin@pixeltech.ru",subject:email)
 	end
 end
