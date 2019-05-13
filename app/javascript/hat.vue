0bbdf27ba101e8ca6c68c72d529971980ebdc4ef
bananvyhe <loadonden@yahoo.com>
2019-05-13 18:45:45 +0500

    responsive pp

--- app/javascript/hat.vue
+++ app/javascript/hat.vue
@@ -1,9 +1,8 @@
 <template>
   <div class="hat fog">
+  	<div class="clouds"></div>
   	<div class="warriors"></div>
- 
-     
-  </div>
+</div>
 </template>
 <script>
  
@@ -24,9 +23,18 @@
 @import "_variables";
 @import "_extends";
 .hat {
+	display: flex;
+	align-items: stretch;
+	position: relative;
 	lost-center: $lcenter;
 	/*lost-utility: edit;*/
 	height: 162px;
+	@media (--only-small-screen) {
+		height: 120px;
+  }
+ 	@media (--only-xsmall-screen) {
+		height: 80px;
+  }
 }
 .fog {
 	background-repeat: no-repeat;
@@ -36,11 +44,34 @@
 }
 .warriors {
 	background-repeat: no-repeat;
-
 	background-position: center bottom;
-		background-size: contain;
+	background-size: contain;
 	height: 100%;
 	width: 100%;
 	background-image: url('./images/_hat/warriors.gif');
+	@media (--only-1600more-screen) {
+		background-size: auto 90%;
+    background-position: center 25px;
+  }
+  @media (--only-medium-screen) {
+		background-size: auto 100%;
+    background-position: center 25px;
+  }
+  @media (--only-small-screen) {
+		background-size: auto 100%;
+    background-position: center 25px;
+  }
+}
+.clouds {  
+	background-repeat: repeat-x;
+	position: absolute;
+	background-image: url('./images/_hat/clouds.png');
+	background-position: center top;
+	background-size: 35%  auto;
+	height: 100%;
+	width: 100%;
+	@media (--only-xsmall-screen) {
+			background-size: 65%  auto;
+  }
 }
 </style>
