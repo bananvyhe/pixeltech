2df0ceaa4512afb7811e6dddcf92327151feae57
bananvyhe <loadonden@yahoo.com>
2019-05-16 18:40:29 +0500

    flash

--- app/javascript/hat.vue
+++ app/javascript/hat.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="hat">
   	<div class="fog"></div>
+  	<div class="flash"></div>
   	<div class="clouds"></div>
   	<div class="warriors"></div>
 	</div>
@@ -10,6 +11,7 @@
   export default {
     data: function (){
       return {
+      	repDelay: 8
         
       }
     },
@@ -17,10 +19,42 @@
     },
     mounted() {
     	var tlwar = new TimelineMax();
-    	tlwar.from('.warriors', 0.5, {backgroundPositionY: 110, ease: Power2.easeOut}, '+=0.6');
+    	tlwar.from('.warriors', 1.2, {backgroundPositionY: 150, ease: Power2.easeOut}, '+=0.8');
     	 
     	var tl2 = new TimelineMax({repeat:-1});
-			tl2.to('.clouds', 90, {backgroundPositionX: 149, ease: Power0.easeOut});
+			tl2.to('.clouds', 90, {backgroundPositionX: 149, ease: Expo.easeOut});
+
+			var repDelay = 8;
+			var firsFlash = 0.2;
+			var flashTime = 0.5;
+			// var repDelay = 8;
+			// var self = this;
+			// function randomize() {
+			// 	var min = 3.0
+			// 	var max = 12.0
+			// 	var repDelay = Math.random() * (max - min) + min;
+			// 	self.repDelay =  Math.round(repDelay)
+			// 	console.log(repDelay)
+			// }
+
+			var tlfl = new TimelineMax({repeat:-1, repeatDelay: repDelay});
+			tlfl.to('.warriors', firsFlash, {css:{className:'+=brightness'}}, '+=2.6')
+			.to('.warriors', 0.15, {css:{className:'-=brightness'}})
+			.to('.warriors', flashTime, {css:{className:'+=brightness'}}, '+=0.1')
+			.to('.warriors', 0.15, {css:{className:'-=brightness'}});
+
+			var tlfl1 = new TimelineMax({repeat:-1, repeatDelay: repDelay});
+			tlfl1.to('.clouds', firsFlash, {css:{className:'+=brightness'}}, '+=2.6')
+			.to('.clouds', 0.15, {css:{className:'-=brightness'}})
+			.to('.clouds', flashTime, {css:{className:'+=brightness'}}, '+=0.1')
+			.to('.clouds', 0.15, {css:{className:'-=brightness'}});
+ 
+			var tldn1 = new TimelineMax({repeat:-1, repeatDelay: repDelay});
+			tldn1.set('.flash', {visibility: "hidden"})
+			.to('.flash', firsFlash, {visibility: "visible"}, '+=2.6')
+			.to('.flash', 0.15, {visibility: "hidden"})
+			.to('.flash', flashTime, {visibility: "visible"}, '+=0.1')
+			.to('.flash', 0.15, {visibility: "hidden"});
     }
 
 
@@ -60,6 +94,29 @@
 	background-image: url('./images/_hat/fog.jpg');
 
 }
+.brightness{
+	filter: brightness(0%);
+}
+
+.flash {
+ 	/*visibility: hidden;*/
+	position: absolute;
+	height: 100%;
+	width: 100%;
+	background-repeat: no-repeat;
+	background-position: center bottom;
+	background-size: cover;
+	background-image: url('./images/_hat/flash.jpg');
+	@media (--only-1600more-screen) {
+    background-position: center -20px;
+  }
+  @media (--only-medium-screen) {
+    background-position: center -20px;
+  }
+}
+.dvis {
+	visibility: visible;
+}
 .warriors {
 	z-index: 20;
 	background-repeat: no-repeat;
@@ -86,7 +143,7 @@
 	position: absolute;
 	background-image: url('./images/_hat/clouds.png');
 	background-position: center top;
-	background-size: 30%  auto;
+	background-size: 40%  auto;
 	height: 100%;
 	width: 100%;
 	@media (--only-xsmall-screen) {
