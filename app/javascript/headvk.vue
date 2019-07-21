9d896849b28067b08ef07b08cfe400d974fa496d
bananvyhe <loadonden@yahoo.com>
2019-07-21 23:31:51 +0500

    fix

--- app/javascript/headvk.vue
+++ app/javascript/headvk.vue
@@ -228,12 +228,10 @@ var cmp = {
         this.gamesendplus({
           amount: loa
         })
-
-        this.crysendplus({
-          amount: Number(response.headers.cry)
-        })          
-
         if (response.headers.cry){
+          this.crysendplus({
+            amount: Number(response.headers.cry)
+          })   
           this.$notify({
             // title: 'Опыт',
             message: 'получено кристаллов: '+response.headers.cry,
