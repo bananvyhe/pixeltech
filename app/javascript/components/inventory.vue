66fc87e21a1639844caf5618c32ebdfde46b6867
bananvyhe <loadonden@yahoo.com>
2019-08-08 21:07:54 +0500

    1

--- app/javascript/components/inventory.vue
+++ app/javascript/components/inventory.vue
@@ -6,7 +6,14 @@
       trigger="click"
       content="this is content, this is content, this is content">
       <div class="slots">
-        <div class="cry" v-if="$store.getters.cry">{{$store.getters.cry}}</div>
+        <el-tooltip  class="smalltext" effect="light" placement="bottom">
+          <div slot="content">
+            <div class="tooldrop">камни вам понадобятся для оплаты специальных действий 
+            </div>
+          </div>
+          <div class="cry" v-if="$store.getters.cry">{{$store.getters.cry}}</div>
+        </el-tooltip>
+       
       </div>
       
       <el-button slot="reference" icon="el-icon-menu" @click="isOpen = ! isOpen"  type="info"  size="mini" circle></el-button>   
@@ -26,6 +33,12 @@ export default {
 </script>
 <style scoped>
 @import "stylesheets/_variables";
+.tooldrop{
+  text-align: center;
+  width: 15em;
+  margin: -0.4em -0.2em;
+  font-size: 0.9em;
+}
 .slots{
   box-shadow: inset 0px 0px 8px rgba(0,0,0,0.3);
   padding: 0.5em 0.5em;
