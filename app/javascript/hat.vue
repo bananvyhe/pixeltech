b68e56bf4afdf080d0ace2b74589499433c558a8
bananvyhe <loadonden@yahoo.com>
2019-08-12 16:48:49 +0500

    1

--- app/javascript/hat.vue
+++ app/javascript/hat.vue
@@ -43,7 +43,7 @@
     	// console.log('22222updatedHat')
 			var vood = new TimelineMax();
 			vood.to('.voodoo', 0.4, { webkitFilter:"brightness(1)",
-          filter:"brightness(1)",y: -48, opacity: 1, scale: 0.12,  ease: Expo.easeIn}, '+=2.5').to('.voodoo', 1.0, {webkitFilter:"brightness(0.8)",
+          filter:"brightness(1)",y: -48, opacity: 1, scale: 0.12,  ease: Expo.easeIn}, '+=1.3').to('.voodoo', 1.0, {webkitFilter:"brightness(0.8)",
           filter:"brightness(0.1)", y:  -20, opacity: 0.3, x: 0, scale: 0.24, ease: Quart.easeIn}, '+=0.9' ).to('.voodoo', 0.1, {webkitFilter:"brightness(0.9)",
           filter:"brightness(0.9)", y:  -21, scale: 1, opacity: 0, x: 0, ease: Quad.easeIn}, '+=0.0' );
 
