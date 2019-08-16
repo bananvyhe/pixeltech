439aaebf9269d62a5d7b181b143f18aa58979680
bananvyhe <loadonden@yahoo.com>
2019-08-16 19:39:14 +0500

    pk system begin implementation

--- app/controllers/api/v1/gameboards_controller.rb
+++ app/controllers/api/v1/gameboards_controller.rb
@@ -26,10 +26,34 @@ class Api::V1::GameboardsController < ApiController
   # POST /gameboards
   # POST /gameboards.json
   def kill 
+    #расчет снятия 4% с уровня при смерти
+    @explvling = [68, 295, 805, 1716, 3154, 5249, 8136, 11955, 16851, 22973, 30475, 39516, 50261, 62876, 77537, 94421, 113712, 135596, 160266, 84495, 95074, 107905, 123472, 142427, 165669, 194509, 231086, 279822, 374430, 209536, 248781, 296428, 354546, 425860]
     pksend =params[:killid]
     @killeduser = Gameboard.find_by_user_id(pksend)
     @killeduser.pk = true
-    @killeduser.save
+    @expuser = @killeduser.expirience
+    @lvlcounter = 0
+    @expcounter = 0
+
+    @explvling.each do |pos|
+      if pos+@expcounter < @expuser
+        @lvlcounter+= 1
+        @expcounter+=pos
+      end
+      if pos+@expcounter > @expuser
+        @nowexp = @explvling[@lvlcounter]
+       
+        @nowexp = @nowexp.to_i   
+            puts @nowexp  
+        @minusedexp = @nowexp / 100 * 4
+        puts @minusedexp
+        @killeduser.expirience -= @minusedexp 
+        @killeduser.save
+        break
+      end
+    end
+
+   
   end
 
   private
