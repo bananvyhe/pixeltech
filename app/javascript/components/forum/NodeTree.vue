abcf26b23331345d3c3966dc340d953f1bef0ca5
bananvyhe <loadonden@yahoo.com>
2019-11-01 15:13:46 +0500

    recursion vue comments tree has working

---
+++ app/javascript/components/forum/NodeTree.vue
@@ -0,0 +1,20 @@
+<template>
+  <li class="node-tree">
+    <span class="label">{{ node.body  }}</span>
+  <!-- {{typeof node }} -->
+<!--     <ul v-if="node.comments && node.comments.length">
+      <node v-for="child in node.comments" :node="child">
+        
+      </node>
+    </ul> -->
+  </li>
+</template>
+
+<script>
+export default {
+  name: "node",
+  props: {
+    node: Object
+  }
+};
+</script>
