996c735c328109c642bd0fa68b5019b4a42c3f2f
bananvyhe <loadonden@yahoo.com>
2019-11-15 17:37:28 +0500

    solved. problem was exist with name controller?

--- app/controllers/api/v1/vueposts_controller.rb
+++ app/controllers/api/v1/vueposts_controller.rb
@@ -25,8 +25,8 @@ class Api::V1::VuepostsController < ApiController
   def shocomm
 
     # print '==========||||||>'
-    # @postComm = Post.find(params[:id]).comments
-        @postComm = 'dfdfdfdfdf'
+    @postComm = Post.find(params[:id]).comments
+        # @postComm = 'dfdfdfdfdf'
 
 # @postComm = @postComm.to_a
     # render json: @postComm
