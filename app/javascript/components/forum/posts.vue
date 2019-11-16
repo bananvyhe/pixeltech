48edb81a7cf6dd451efa8fa71a0ab0dec36d096e
bananvyhe <loadonden@yahoo.com>
2019-11-16 15:40:47 +0500

    1

--- app/javascript/components/forum/posts.vue
+++ app/javascript/components/forum/posts.vue
@@ -7,12 +7,13 @@
     </div>
     <!-- <showpost :number='number'></showpost> -->
     <newpost :section='this.$store.getters.role.role'></newpost>
+    
     <div v-if="this.$store.getters.role.role != 'user'">
       <h2>Общий доступ</h2>
       <div v-for="(item, index) in userposts">
         <showpost :number='item.id' :body='item.body' :title='item.title' :username='item.title'></showpost>
       </div>
-      <newpost :section='"user"'></newpost>
+      <!-- <newpost :section='"user"'></newpost> -->
     </div>
   </div>
 </template>
@@ -52,7 +53,7 @@ export default {
           })
           .then((response) => {
             console.log(response.data);
-             this.userposts = response.data
+            this.userposts = response.data
           })
           .catch(function (error) {
             console.log(error);
