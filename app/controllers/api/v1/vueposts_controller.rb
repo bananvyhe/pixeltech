ec1acfa45f6e3bad64c97e3fc99ced1fb37b2ae9
bananvyhe <loadonden@yahoo.com>
2019-11-20 20:29:14 +0500

    1

--- app/controllers/api/v1/vueposts_controller.rb
+++ app/controllers/api/v1/vueposts_controller.rb
@@ -28,7 +28,7 @@ class Api::V1::VuepostsController < ApiController
     print '000000000000>'
     # @postComm = Post.includes(:user).find(params[:id]).comments
     # @postComm = ['1','22','3','4','5']
-    @postComm = Post.find(params[:id]).comments.includes(:user)
+    @postComm = Post.includes(:user).find(params[:id]).comments
     puts @postComm.inspect
     print '000000000000>'
 
