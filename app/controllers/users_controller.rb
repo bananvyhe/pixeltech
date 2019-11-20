0baba72039eaaf6790a0acfeeb03bd6f63094bed
bananvyhe <loadonden@yahoo.com>
2019-11-20 12:59:17 +0500

    1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,11 @@
-class UsersController < ApplicationController
+class UsersController < Api2Controller
     # before_action :authenticate_user!
-		before_action :set_user #, only: [:show, :edit, :update, :destroy]
+		before_action :set_user, only: [:show, :edit, :update, :destroy]
   def index
   	@users = User.all.order(created_at: :desc)
      
+  end
+  def show
   end
   def create
 
