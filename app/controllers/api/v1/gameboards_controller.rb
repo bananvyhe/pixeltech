1636217a95178c508e53b3e8cdaf4b4140f73dc1
bananvyhe <loadonden@yahoo.com>
2020-05-13 15:34:25 +0500

    remove res

--- app/controllers/api/v1/gameboards_controller.rb
+++ app/controllers/api/v1/gameboards_controller.rb
@@ -128,12 +128,12 @@ class Api::V1::GameboardsController < ApiController
       
         if plusi.count == 2 &&  (@userplus.has_any_role? :user)
           @make_clan_item =  ItemAttribute.find_or_create_by(item_name: 'Права лидера', description: 'Позволяет создать клан', image: '../images/clanbuild.jpg') 
-          @make_res_item =  ItemAttribute.find_or_create_by(item_name: 'Воскрешение', description: 'Позволяет воскресить союзника', image: '../images/res.jpg') 
+          # @make_res_item =  ItemAttribute.find_or_create_by(item_name: 'Воскрешение', description: 'Позволяет воскресить союзника', image: '../images/res.jpg') 
           @items = MyItem.new(item_attribute: @make_clan_item, user: @userplus, qty: 1)
-          @items1 = MyItem.new(item_attribute: @make_res_item, user: @userplus, qty: 0)
-          # @items.item_attribute << @make_clan_item
+          # @items1 = MyItem.new(item_attribute: @make_res_item, user: @userplus, qty: 0)
           @items.save!
-          @items1.save!
+          # @items1.save!
+
           # clname = Clan.find_by(clan: "superadmin")
           clname = Role.find_by(name: "superadmin")
           @adname = User.find_by(role: "superadmin")
