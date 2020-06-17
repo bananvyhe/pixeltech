efb2b5183bbecd391f2c3f50440e7b3f4eea6e7a
bananvyhe <loadonden@yahoo.com>
2020-06-17 23:14:16 +0500

    separating userinfo is done

--- app/javascript/components/userinfo.vue
+++ app/javascript/components/userinfo.vue
@@ -1,17 +1,96 @@
 <template>
- 
+  <div>
+  <div class="userinfoplace">
+    <el-popover
+      v-if="userplus"
+      placement="top"
+      width=""
+      trigger="hover">
+      <div class="tinytext">
+        {{userplus}}
+      </div>
+      <div slot="reference">
+          за: {{plusvotes}}
+      </div>
+    </el-popover> 
+    <div v-else>
+      за: 0
+    </div>
+    <el-popover
+      v-if="userminus"
+      placement="top"
+      width=""
+      trigger="hover">
+      <div class="tinytext">
+        {{userminus}}                  
+      </div>
+      <div slot="reference">
+          против: {{minusvotes}}
+      </div>
+    </el-popover>                 
+    <div v-else>
+      против: 0
+    </div>    
+
+    <div v-if="userkarma != 0">
+      карма: -{{userkarma}}
+    </div>           
+  </div>
+  <div class="userinterface ">
+    <div>
+      <el-button label="2" border size="mini"  class="aprior">Сообщение</el-button>
+    </div>
+    <div>
+      <el-tooltip placement="top">
+        <div slot="content" class="smalltext notif"><i>плюс в карму<br>+7 открывают<br> возможности лидера</i> 
+        </div>
+        <el-button @click="voteplus(item.id)" type="warning"  label="4" border size="mini"  class="aprior"> + </el-button>
+      </el-tooltip>
+    </div>
+    <div>
+      <el-tooltip placement="top">
+        <div slot="content"  class="smalltext notif" >минус в карму
+        </div>
+        <el-button  @click="vote(item.id)" type="warning"  label="3" border size="mini"  class="aprior"> - </el-button>
+      </el-tooltip>
+    </div>
+    <div>
+
+      <el-tooltip  v-if="$store.getters.cry >= 100" placement="top">
+        <div slot="content"  class="smalltext notif plashka2"> <span style="color: green;">стоимость 100 камней</span> <br>   нейтрализует оппонента <br> <span style="color: red;"><i>ваша карма будет испорчена</i></span>   </div>
+        <el-button type="danger" label="1" bordser size="mini" class="aprior pk" @click='pkconfirm(item.id)'> 
+          ПК 
+        </el-button>
+      </el-tooltip>
+      <el-tooltip v-else  placement="top">
+        <div slot="content"  class="smalltext notif"> 
+          <span style="color: green;">стоимость 100 камней</span> <br> <i> нейтрализует оппонента <br> <span style="color: red;">ваша карма будет испорчена</span> </i> 
+        </div>
+        <el-button disabled type="danger" label="1" border size="mini" class="aprior"> 
+          ПК 
+        </el-button>
+      </el-tooltip>
+    </div>    
+  </div>
+  </div>
 </template>
 
 <script>
 import axios from 'axios'
 export default {
+  // props: ['id'],
   data: function () {
     return {
-      
+      visible: false,
+      userkarma: '',
+      plusvotes: '',
+      userplus: [],
+      minusvotes: '',
+      userminus: [],
     };
   },
   watch: {
-     
+    
   }, 
   computed: {
 
@@ -20,12 +99,60 @@ export default {
      
   },
   methods: {
-      
+    getInfoUser(id){
+      axios({
+        method: 'get',
+        url: '/api/v1/userinfo',
+        headers: {
+          'Authorization': 'bearer '+this.$store.getters.token.access
+        }, 
+        params: {
+          user_id: id
+        }
+      })
+      .then((response) => {        
+        var totalplus = response.headers.userplus
+        var arrsizeminus = response.headers.arraysizemin
+        var totalminus = response.headers.usermin
+        var arrsizeplus = response.headers.arraysizeplus
+        var userkarma = response.headers.userkarma
+        this.userminus = totalminus
+        this.minusvotes = arrsizeminus
+        this.userplus = totalplus
+        this.plusvotes = arrsizeplus
+        this.userkarma = userkarma
+      });
+    },      
  
   }
 }
 </script>
 <style scoped>
- 
+.userinfoplace {
+  display: flex;
+  flex-direction: row;
+  /*background-color: #dad;*/
+  padding: 0em;
+  margin-bottom: 1em;
+  div {
+    margin: 0em 0.5em;
+    /*background-color: #dad;*/
+  }
+}
+.userinterface {
+  padding: 0 0.2em;
+  margin: -0.9em;
+  display: flex;
+  flex-direction: row;
+  div {
+   padding: 0.2em 0.0em ;
+  }
+}
+.notif {
+  font-weight: 300;
+  text-align: center;
+  line-height: 1.4em;
+}
+
 
 </style>
--- app/javascript/lobby.vue
+++ app/javascript/lobby.vue
@@ -9,95 +9,21 @@
       <!-- {{liveusers}}<br><br> -->
       <!-- {{data}}  -->
       <el-button-group class="users"  size="mini" >
-        <div v-for="(item, index) in liveusers">
-        <!-- <el-badge :value="item.id" class="mark" size="small"> -->
+        <div v-for="(item, index) in liveusers" :key="item.id" >
+
           <el-popover
             placement="bottom"
             width=""
             trigger="hover"
             >
-            <div class="userinfoplace">
-
-              <el-popover
-                v-if="userplus"
-                placement="top"
-                width=""
-                trigger="hover">
-                <div class="tinytext">
-                  {{userplus}}
-                </div>
-                <div slot="reference">
-                    за: {{plusvotes}}
-                </div>
-              </el-popover> 
-              <div v-else>
-                за: 0
-              </div>
-              <el-popover
-                v-if="userminus"
-                placement="top"
-                width=""
-                trigger="hover">
-                <div class="tinytext">
-                  {{userminus}}                  
-                </div>
-                <div slot="reference">
-                    против: {{minusvotes}}
-                </div>
-              </el-popover>                 
-              <div v-else>
-                против: 0
-              </div>    
-
-              <div v-if="userkarma != 0">
-                карма: -{{userkarma}}
-              </div>           
-            </div>
-<!--              <div class="userinfo smalltext">
-                голоса: {{item.plus - item.minus}}                
-              </div> -->
-            <div class="userinterface ">
-              <div>
-                <el-button label="2" border size="mini"  class="aprior">Сообщение</el-button>
-              </div>
-              <div>
-                <el-tooltip placement="top">
-                  <div slot="content" class="smalltext notif"><i>плюс в карму<br>+7 открывают<br> возможности лидера</i> 
-                  </div>
-                  <el-button @click="voteplus(item.id)" type="warning"  label="4" border size="mini"  class="aprior"> + </el-button>
-                </el-tooltip>
-              </div>
-              <div>
-                <el-tooltip placement="top">
-                  <div slot="content"  class="smalltext notif" >минус в карму
-                  </div>
-                  <el-button  @click="vote(item.id)" type="warning"  label="3" border size="mini"  class="aprior"> - </el-button>
-                </el-tooltip>
-              </div>
-              <div>
-
-                <el-tooltip  v-if="$store.getters.cry >= 100" placement="top">
-                  <div slot="content"  class="smalltext notif plashka2"> <span style="color: green;">стоимость 100 камней</span> <br>   нейтрализует оппонента <br> <span style="color: red;"><i>ваша карма будет испорчена</i></span>   </div>
-                  <el-button type="danger" label="1" bordser size="mini" class="aprior pk" @click='pkconfirm(item.id)'> 
-                    ПК 
-                  </el-button>
-                </el-tooltip>
-                <el-tooltip v-else  placement="top">
-                  <div slot="content"  class="smalltext notif"> 
-                    <span style="color: green;">стоимость 100 камней</span> <br> <i> нейтрализует оппонента <br> <span style="color: red;">ваша карма будет испорчена</span> </i> 
-                  </div>
-                  <el-button disabled type="danger" label="1" border size="mini" class="aprior"> 
-                    ПК 
-                  </el-button>
-                </el-tooltip>
-              </div>    
-            </div>
+            <userinfo :ref="item.id" ></userinfo>
             <el-button v-if="item.dead == false" slot="reference" class="user" border>
               <div class="item" @mouseover="mouseOverUser(item.id)" v-bind:style="item.karma > 0 ? 'color: red;' : ''">
                 {{item.username}}
               </div> 
             </el-button>        
           </el-popover>
+
 
           <el-dialog
             title="Вы уверены?"
@@ -129,11 +55,7 @@ export default {
   data: function () {
     return { 
       exptime: '',
-      userkarma: '',
-      plusvotes: '',
-      userplus: [],
-      minusvotes: '',
-      userminus: [],
+
       pkid: '',
       dialogVisible: false,
       radio: '1',
@@ -152,11 +74,10 @@ export default {
       }         
     },50 );  
     // setTimeout(function(){
-
     // },4800 ); 
   },
   components: {
-      'userinfo': userinfo
+    'userinfo': userinfo
   },  
   watch: {
     exptime() {
@@ -169,10 +90,7 @@ export default {
     }
   }, 
   computed: {
-
-
   },
- 
   methods: {
     // ...mapMutations([
     //   'crysendplus'
@@ -180,34 +98,31 @@ export default {
     checkRelevanceToken(){
       this.exptime = this.$store.getters.role.exp - new Date().getTime()/1000
     },
-    mouseOverUser(id){
-      axios({
-        method: 'get',
-        url: '/api/v1/userinfo',
-        headers: {
-          'Authorization': 'bearer '+this.$store.getters.token.access
-        }, 
-        params: {
-          user_id: id
-        }
-        // params: {
-        //   rait: this.value,
-        //   pos: this.pos
-        // } 
-        })
-        .then((response) => {        
-          var totalplus = response.headers.userplus
-          var arrsizeminus = response.headers.arraysizemin
-          var totalminus = response.headers.usermin
-          var arrsizeplus = response.headers.arraysizeplus
-          var userkarma = response.headers.userkarma
-          this.userminus = totalminus
-          this.minusvotes = arrsizeminus
-          this.userplus = totalplus
-          this.plusvotes = arrsizeplus
-          this.userkarma = userkarma
-      });
-     
+    mouseOverUser(ref){
+      // console.log(ref)
+      this.$refs[ref][0].getInfoUser(ref)
+    //   axios({
+    //     method: 'get',
+    //     url: '/api/v1/userinfo',
+    //     headers: {
+    //       'Authorization': 'bearer '+this.$store.getters.token.access
+    //     }, 
+    //     params: {
+    //       user_id: id
+    //     }
+    //   })
+    //   .then((response) => {        
+    //     var totalplus = response.headers.userplus
+    //     var arrsizeminus = response.headers.arraysizemin
+    //     var totalminus = response.headers.usermin
+    //     var arrsizeplus = response.headers.arraysizeplus
+    //     var userkarma = response.headers.userkarma
+    //     this.userminus = totalminus
+    //     this.minusvotes = arrsizeminus
+    //     this.userplus = totalplus
+    //     this.plusvotes = arrsizeplus
+    //     this.userkarma = userkarma
+    //   });
     },
     vote(id){
       axios({
@@ -345,17 +260,7 @@ export default {
 .uiframe{
   padding: 0.5em 0 0;
 }
-.userinfoplace {
-  display: flex;
-  flex-direction: row;
-  /*background-color: #dad;*/
-  padding: 0em;
-  margin-bottom: 1em;
-  div {
-    margin: 0em 0.5em;
-    /*background-color: #dad;*/
-  }
-}
+
 .item {
   margin: -0.5em -1em;
 }
@@ -364,20 +269,7 @@ export default {
    margin: -0.9em 0;
   padding: 1em;
 }
-.notif {
-  font-weight: 300;
-  text-align: center;
-  line-height: 1.4em;
-}
-.userinterface {
-  padding: 0 0.2em;
-  margin: -0.9em;
-  display: flex;
-  flex-direction: row;
-  div {
-   padding: 0.2em 0.0em ;
-  }
-}
+
 .user {
   padding: 0.9em;
   margin: 0.2em 0.2em;
