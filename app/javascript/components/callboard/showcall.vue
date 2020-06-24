9af784a1c782f8be5ee0e5b44eff6469c7de954a
bananvyhe <loadonden@yahoo.com>
2020-06-24 21:56:21 +0500

    callboard pp

--- app/javascript/components/callboard/showcall.vue
+++ app/javascript/components/callboard/showcall.vue
@@ -1,7 +1,28 @@
 <template>
   <div>
     <div class="callpost">
-      {{this.title}}{{this.body}}{{this.username}}       
+      <div class="flexcontainer">
+        <div class="top">
+          <div class="img">
+            img
+          </div>
+          <div class="info">
+            <div class="middle">
+              {{this.title}}{{this.body}}{{this.username}}            
+            </div>
+            <div class="end">
+              234
+            </div>               
+          </div>
+     
+        </div>
+
+        <div class="bottom">
+          bot
+        </div>        
+      </div>  
+
+   
     </div>
 
   </div>
@@ -44,8 +65,41 @@ export default {
 </script>
 <style scoped>
 @import "../../stylesheets/_variables";
+.flexcontainer{
+  height: 100%;
+
+}
+.top{
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  lost-row: 2/3; 
+  /*background-color: #ada; */
+}
+.bottom{
+  lost-row: 1/3;
+  /*background-color: #dad; */
+}
+.img{
  
+  /*height: 6em;*/
+  width: 6em;
+}
+.info{
+width: 100%;
+}
+.middle{
+  lost-column: 1/2 2 0em;
+}
+.end{
+  lost-column: 1/2 2 0em;
+}
 .callpost{
+  /*display: flex;*/
+  /*flex-direction: column;*/
+
+  /*align-items: stretch;*/
+ lost-utility: edit;
   height: 10em;
   background-color: $bgcol;
   margin: 0.5em ;
