d1d1db0514fdce6ef01fcbb19d4c0e1136198c2a
bananvyhe <loadonden@yahoo.com>
2020-07-04 23:34:43 +0500

    1

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,8 +1,8 @@
 Sidekiq::Extensions.enable_delay!
 Sidekiq.configure_server do |config|
-	config.redis = { url: 'redis://localhost:6379/sidekiq_test'}
+	config.redis = { url: 'redis://localhost:6379/sidekiq_test', namespace: "sidekiq_pixeltech"}
 end
 
 Sidekiq.configure_client do |config|
-	config.redis = { url: 'redis://localhost:6379/sidekiq_test'}
+	config.redis = { url: 'redis://localhost:6379/sidekiq_test', namespace: "sidekiq_pixeltech"}
 end
