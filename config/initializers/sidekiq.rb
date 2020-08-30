23a469e9461738327fa53fa8c82726e4496a46d2
deploy <loadonden@yahoo.com>
2020-08-30 20:04:42 +0500

    last working ver

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,9 +1,19 @@
 Sidekiq::Extensions.enable_delay!
+	if Rails.env.development? 
+		Sidekiq.configure_server do |config|
+			config.redis = { url: 'redis://127.0.0.1:6379/sidekiq_test', namespace: "sidekiq_pixeltech" }
+		end
 
-	Sidekiq.configure_server do |config|
-		config.redis = { url: 'redis://127.0.0.1:6380/sidekiq_test', namespace: "sidekiq_pixeltech" }
+		Sidekiq.configure_client do |config|
+			config.redis = { url: 'redis://127.0.0.1:6379/sidekiq_test', namespace: "sidekiq_pixeltech" }
+		end
 	end
+		if Rails.env.production? 
+		Sidekiq.configure_server do |config|
+			config.redis = { url: 'redis://127.0.0.1:6380/sidekiq_test', namespace: "sidekiq_pixeltech" }
+		end
 
-	Sidekiq.configure_client do |config|
-		config.redis = { url: 'redis://127.0.0.1:6380/sidekiq_test', namespace: "sidekiq_pixeltech" }
+		Sidekiq.configure_client do |config|
+			config.redis = { url: 'redis://127.0.0.1:6380/sidekiq_test', namespace: "sidekiq_pixeltech" }
+		end
 	end
