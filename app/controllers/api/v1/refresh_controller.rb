16445742fd2e7845e9cd263b1ac1b093f29c2adb
deploy <loadonden@yahoo.com>
2020-09-20 21:58:42 +0500

    change api to api2 controller on refresh

--- app/controllers/api/v1/refresh_controller.rb
+++ app/controllers/api/v1/refresh_controller.rb
@@ -1,15 +1,16 @@
-class Api::V1::RefreshController < ApiController
-   before_action :authorize_refresh_request!
-   skip_before_action :authenticate_user!, raise: false
+class Api::V1::RefreshController < Api2Controller
+   # skip_before_action :verify_authenticity_token
+    before_action :authorize_refresh_request!, raise: false
+   # skip_before_action :authenticate_user!, raise: false
   def create
  
-    
+     puts 'create--------------'
     tokens = JWTSessions::Session.new(payload: access_payload , refresh_payload:  refresh_payload  )
     render json: tokens.login
 
   end
   def access_payload
-
+    puts 'access_payload--------------'
     # payload here stands for refresh token payload
      user = User.find_by!(id: payload['user_id'])
     allroles = user.roles 
@@ -20,11 +21,13 @@ class Api::V1::RefreshController < ApiController
     { user_id: user.id, role: @roles, username: user.username}
   end
   def refresh_payload
+    puts 'refresh_payload--------------'
     # payload here stands for refresh token payload
      user = payload['user_id']
     { user_id: user}
   end
       def current_user
+        puts 'current_user--------------'
     @current_user ||= User.find(payload['user_id'])
   end
 
