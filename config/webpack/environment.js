a999714eaf0e10a10833163e17cac2484530e48d
deploy <loadonden@yahoo.com>
2020-09-24 21:55:32 +0500

    vuetify loader retrieve

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -4,23 +4,24 @@ const vue = require('./loaders/vue')
 const scss = require('./loaders/scss')
 
  //после активации вьютифай лоадер плагина отключается variables.scss файл :(
-// const VuetifyLoaderPlugin = require('vuetify-loader/lib/plugin')
-// environment.plugins.prepend('VuetifyLoaderPlugin', new VuetifyLoaderPlugin())
+const VuetifyLoaderPlugin = require('vuetify-loader/lib/plugin')
+environment.plugins.prepend('VuetifyLoaderPlugin', new VuetifyLoaderPlugin())
 
-
-
-environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
-environment.loaders.prepend('vue', vue)
 // environment.loaders.prepend('scss', scss)
 // Get the actual sass-loader config
 const sassLoader = environment.loaders.get('sass')
 const sassLoaderConfig = sassLoader.use.find(function(element) {
   return element.loader == 'sass-loader'
 })
+
 // Use Dart-implementation of Sass (default is node-sass)
 const options = sassLoaderConfig.options
 options.implementation = require('sass')
 options.additionalData = `@import "app/scss/variables.scss"`
+
+environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
+
+environment.loaders.prepend('vue', vue)
 
 module.exports = environment
  
