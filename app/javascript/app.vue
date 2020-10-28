d16c592145bc7e71068ad738a25d09592372f0d4
deploy <loadonden@yahoo.com>
2020-10-28 19:42:40 +0500

    3

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,47 +1,49 @@
 <template>
   <div  class="vmain">
     <v-app >
-      <v-app-bar app
-      class="appbar"
-      dense>
-        <v-app-bar-nav-icon>
-        </v-app-bar-nav-icon>
-        <v-toolbar-title>
-          Page title
-        </v-toolbar-title>
-        <v-spacer></v-spacer>
-        <div class="wrap" v-if="$store.getters.token == null" > 
-          <reg></reg>
-        </div>
-        <div class="wrap">
-          <log ></log>
-        </div>
-        <v-btn icon>
-          <v-icon>mdi-heart</v-icon>
-        </v-btn>
-        <v-btn icon>
-          <v-icon>mdi-magnify</v-icon>
-        </v-btn>     
-        <v-menu
-          >
-          <template v-slot:activator="{ on, attrs }">
-            <v-btn
-              icon
-              v-bind="attrs"
-              v-on="on">
-              <v-icon>mdi-dots-vertical</v-icon>
-            </v-btn>
-          </template>
-          <v-list>
-            <v-list-item
-              v-for="n in 5"
-              :key="n"
-              @click="() => {}">
-              <v-list-item-title>Option {{ n }}</v-list-item-title>
-            </v-list-item>
-          </v-list>
-        </v-menu>         
-      </v-app-bar>
+      <v-container>
+        <v-app-bar 
+        hide-on-scroll
+        dense>
+          <v-app-bar-nav-icon>
+          </v-app-bar-nav-icon>
+          <v-toolbar-title>
+            Page title
+          </v-toolbar-title>
+          <v-spacer></v-spacer>
+          <div class="wrap" v-if="$store.getters.token == null" > 
+            <reg></reg>
+          </div>
+          <div class="wrap">
+            <log ></log>
+          </div>
+          <v-btn icon>
+            <v-icon>mdi-heart</v-icon>
+          </v-btn>
+          <v-btn icon>
+            <v-icon>mdi-magnify</v-icon>
+          </v-btn>     
+          <v-menu
+            >
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn
+                icon
+                v-bind="attrs"
+                v-on="on">
+                <v-icon>mdi-dots-vertical</v-icon>
+              </v-btn>
+            </template>
+            <v-list>
+              <v-list-item
+                v-for="n in 5"
+                :key="n"
+                @click="() => {}">
+                <v-list-item-title>Option {{ n }}</v-list-item-title>
+              </v-list-item>
+            </v-list>
+          </v-menu>         
+        </v-app-bar>
+      </v-container>
       <v-main>
         <div v-if="!$store.getters.role"  class="heado">
           <heado></heado>
@@ -54,36 +56,38 @@
         </div>
         <!-- <posts class="topsidepanel "></posts> -->
       </v-main>
-      <v-footer
-      width="100%"
-      class="appbar"
-      v-bind="localAttrs"
-      :padless="padless">
-        <v-card
-          flat
-          tiless
-          width="100%"
-          class=" text-center">
-<!--           <v-card-text>
-            <v-btn
-              v-for="icon in icons"
-              :key="icon"
-              class="mx-3"
-              icon
-            >
-              <v-icon size="20px">
-                {{ icon }}
-              </v-icon>
-            </v-btn>
-          </v-card-text>
+      <v-container>
+        <v-footer
+        width="100%"
+        class="appbar"
+        v-bind="localAttrs"
+        :padless="padless">
+          <v-card
+            flat
+            tiless
+            width="100%"
+            class=" text-center">
+  <!--           <v-card-text>
+              <v-btn
+                v-for="icon in icons"
+                :key="icon"
+                class="mx-3"
+                icon
+              >
+                <v-icon size="20px">
+                  {{ icon }}
+                </v-icon>
+              </v-btn>
+            </v-card-text>
 
-          <v-divider></v-divider> -->
+            <v-divider></v-divider> -->
 
-          <v-card-text class="white--text">
-            {{ new Date().getFullYear() }} — <strong>Pixeltech</strong>
-          </v-card-text>
-        </v-card>
-      </v-footer>
+            <v-card-text class="white--text">
+              {{ new Date().getFullYear() }} — <strong>Pixeltech</strong>
+            </v-card-text>
+          </v-card>
+        </v-footer>
+      </v-container>
     </v-app>
   </div>
 </template>
