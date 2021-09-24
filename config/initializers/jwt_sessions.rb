9458670ff517e569a2dbecd6c8ec7872413a85e9
bananvyhe <loadonden@yahoo.com>
2021-09-24 19:41:45 +0500

    fix

--- config/initializers/jwt_sessions.rb
+++ config/initializers/jwt_sessions.rb
@@ -3,4 +3,4 @@ JWTSessions.encryption_key = Rails.application.credentials.secret_key_base
 #JWTSessions.refresh_exp_time = 604800 # 1 week in seconds
 JWTSessions.access_exp_time = 300
 JWTSessions.refresh_exp_time = 604800
-JWTSessions.token_store = :redis, { redis_url: "redis://localhost:6380" }
+JWTSessions.token_store = :redis, { redis_url: "redis://127.0.0.1:6380" }
