3e8756237a6aa4fec6d1260c25687d25423ee427
bananvyhe <loadonden@yahoo.com>
2021-09-30 15:26:56 +0500

    fix deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.11.0"
+lock "~> 3.16.0"
 # set :whenever_identifier, ->{ "#{fetch(:application)}_#{fetch(:stage)}" }
 
 # set :sidekiq_processes, 2
