87bf8c520d0ad18a2171544440177bc763fea86f
bananvyhe <loadonden@yahoo.com>
2021-09-30 15:17:44 +0500

    on prod

--- config/schedule.rb
+++ config/schedule.rb
@@ -6,7 +6,7 @@ job_type :sidekiq,  "cd :path && RAILS_ENV=:environment sidekiq-client :task :ou
 # set :output, "/log/cron.log"
 set :output, "#{path}/log/cron.log"
 
-set :environment, :development 
+set :environment, :production 
 
 # every 1.minutes do
 #   rake   output: {error: "#{path}/log/error.log", standard: "#{path}/log/cron.log"}
