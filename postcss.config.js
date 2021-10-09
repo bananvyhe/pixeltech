696538b3a537b1dc9e2f58554ce7925614be63e7
bananvyhe <loadonden@yaho.com>
2021-10-09 21:46:29 +0500

    1

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   plugins: [
   require('postcss-easy-import'),
-    require('postcss-import'),
+    // require('postcss-import'),
         require('postcss-simple-vars'),
     require('postcss-color-function'),
     require('postcss-extend'),
