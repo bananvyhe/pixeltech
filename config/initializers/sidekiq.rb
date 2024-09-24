d942ca0b974973ea982f5eca956b5e022c90fbe5
bananvyhe <loadonden@yahoo.com>
2024-09-24 17:29:59 +0500

    make 2th redis instance

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -7,6 +7,13 @@ Sidekiq::Extensions.enable_delay!
 		Sidekiq.configure_client do |config|
 			config.redis = { url: 'redis://127.0.0.1:6380/sidekiq_test', namespace: "sidekiq_pixeltech" }
 		end
+		Sidekiq.configure_server do |config|
+			config.redis = { url: 'redis://127.0.0.1:6379/sidekiq_test', namespace: "sidekiq_pixeltech" }
+		end
+
+		Sidekiq.configure_client do |config|
+			config.redis = { url: 'redis://127.0.0.1:6379/sidekiq_test', namespace: "sidekiq_pixeltech" }
+		end
  
 	# 	if ENV['RAILS_ENV']=='production'
 	# 	Sidekiq.configure_server do |config|
